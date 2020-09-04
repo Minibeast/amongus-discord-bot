7c5757c04bf99fbe62a7a3e2918a2703676be84c
Minibeast <minibossemail@gmail.com>
2020-09-03 20:51:04 -0400

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+# Among Us Discord Bot
+
+### Invite
+
+The bot is still in development, if any issues arise please report it on GitHub
+
+https://discord.com/oauth2/authorize?client_id=750905866827464825&scope=bot&permissions=1059840
+
+### Commands
+- `!makeroom` - Makes the room. If you enter the command while not in a Voice Channel, you'll be asked to input it's ID. To find Channel IDs, read this: <https://bit.ly/3i0kBBi>. It will just be a string of numbers (Example: `346422907964227587`)
+- `!join` - Join the waiting list
+- `!leave` - Leave the waiting list
+- `!transferowner` - Transfers the room owner to someone else (requires ownership)
+- `!setcode` - Set the Among Us private game code (requires ownership)
+- `!deleteroom` - Deletes the room (requires ownership or manage server perms)
---
+++ main.py
@@ -0,0 +1,183 @@
+import discord
+import sys
+import asyncio
+
+current_rooms = []
+"""
+AmongUs
+"""
+
+
+class AmongUs:
+    def __init__(self, owner, channel):
+        self.channel = channel
+        self.guild = channel.guild
+        self.lobby = channel.members
+        self.waiting = []
+        self.max_players = 10
+        self.owner = owner
+        self.code = ""
+        self.embed = discord.Embed(title="Among Us", type='rich', color=discord.Color.from_rgb(207, 25, 32))
+        self.embed.set_thumbnail(url="https://i.imgur.com/lsXr8my.png")
+        self.embed.set_footer(text="Room Owner: {}".format(self.owner))
+        self.message = None
+
+    def update_max(self, max_players):
+        self.max_players = max_players
+
+    def update_owner(self, owner):
+        self.owner = owner
+
+    def update_code(self, code):
+        self.code = code
+
+    def kick(self, username=None, user=None):
+        print("kick code")
+
+
+async def update_room(room):
+    voice_members = ""
+    room.lobby = room.channel.members
+    for x in room.lobby:
+        voice_members += str(x.name) + "\n"
+
+    if len(voice_members) == 0:
+        voice_members = "No Players"
+
+    room.embed.clear_fields()
+    room.embed.add_field(name="In Lobby ({0}/{1})".format(len(room.lobby), room.max_players), value=voice_members)
+
+    waiting_members = ""
+    for x in room.waiting:
+        if x not in room.lobby:
+            waiting_members += str(x.name) + "\n"
+        else:
+            room.waiting.remove(x)
+
+    if len(waiting_members) == 0:
+        waiting_members = "No Players"
+
+    room.embed.add_field(name="Waiting ({})".format(len(room.waiting)), value=waiting_members)
+
+    room.embed.set_footer(text="Room Owner: {}".format(room.owner))
+
+    if len(room.code) != 0:
+        room.embed.description = "Code: {}".format(room.code)
+
+    try:
+        await room.message.edit(embed=room.embed)
+    except discord.errors.NotFound:
+        current_rooms.remove(room)
+        return
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_voice_state_update(self, member, before, after):
+        for x in current_rooms:
+            if before.channel is not None and before.channel == x.channel:
+                await update_room(x)
+            elif after.channel is not None and after.channel == x.channel:
+                await update_room(x)
+
+    async def on_message(self, message):
+        if message.author == client.user:
+            return
+
+        def check(arg):
+            return message.author == arg.author
+
+        if message.content.startswith("!makeroom"):
+            for x in current_rooms:
+                if x.guild == message.channel.guild:
+                    return
+
+            voice_chan = None
+            game_owner = None
+            for x in message.guild.channels:
+                if type(x) == discord.channel.VoiceChannel:
+                    for i in x.members:
+                        if i == message.author:
+                            game_owner = message.author
+                            voice_chan = x
+
+            if voice_chan is None:
+                error_message = await message.channel.send("User not connected to a channel. Please enter the voice channel ID.")
+                try:
+                    voice_id = await client.wait_for('message', check=check, timeout=30)
+                    game_owner = voice_id.author
+                    voice_chan = await client.fetch_channel(voice_id.content)
+                    await voice_id.delete()
+                    await error_message.delete()
+                except asyncio.TimeoutError:
+                    await message.channel.send("User did not respond within 30 seconds, aborting...")
+                    return
+
+            current = AmongUs(game_owner, voice_chan)
+            current_rooms.append(current)
+
+            current.message = await message.channel.send(embed=current.embed)
+            await update_room(current)
+            # await current_rooms[len(current_rooms) - 1]["message"].pin(reason="Among Us game")
+
+        elif message.content.startswith("!join"):
+            for x in current_rooms:
+                if x.guild == message.channel.guild:
+                    if message.author in x.waiting:
+                        return
+                    else:
+                        x.waiting.append(message.author)
+                        await update_room(x)
+                        return
+
+        elif message.content.startswith("!transferowner"):
+            if len(message.mentions) == 0:
+                await message.channel.send("Please @ the user who you would like to transfer room ownership to.")
+                return
+
+            room = None
+
+            for x in current_rooms:
+                if x.guild == message.channel.guild:
+                    room = x
+
+            if room is None:
+                return
+
+            if message.author == room.owner:
+                room.update_owner(message.mentions[0])
+                await update_room(room)
+            else:
+                await message.channel.send("Only the room owner can change this value!")
+
+        elif message.content.startswith("!leave"):
+            for x in current_rooms:
+                if x.guild == message.channel.guild:
+                    if message.author in x.waiting:
+                        x.waiting.remove(message.author)
+                        await update_room(x)
+                        return
+
+        elif message.content.startswith("!deleteroom"):
+            for x in current_rooms:
+                if x.guild == message.channel.guild:
+                    if message.author == x.owner or message.author.permissions_in(message.channel).manage_guild:
+                        await x.message.delete()
+                        current_rooms.remove(x)
+                        return
+
+        elif message.content.startswith("!setcode"):
+            for x in current_rooms:
+                if x.guild == message.channel.guild:
+                    if message.author == x.owner:
+                        try:
+                            x.code = message.content.split()[1].upper()
+                            await update_room(x)
+                        except IndexError and LookupError:
+                            await message.channel.send("Please enter the code with the command")
+
+
+client = MyClient()
+client.run(str(sys.argv[1]))
